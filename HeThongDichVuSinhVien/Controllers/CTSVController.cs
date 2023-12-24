2ffb67d9a2f93b9932a84241d372e1099dfe3128
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2023-12-24 11:36:39 +0700

    Giao diện CTSV

---
+++ HeThongDichVuSinhVien/Controllers/CTSVController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeThongDichVuSinhVien.Controllers
+{
+    public class CTSVController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
